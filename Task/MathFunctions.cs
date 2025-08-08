3787f591aeaae403f17e215cbeeb083de451e7be
Hazem Mofdi <hazemmofdi11@gmail.com>
2025-08-08 21:07:09 +0300

    Task

---
+++ Task/MathFunctions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    public delegate int MathDelegate(int num1, int num2);
+    internal class MathFunctions
+    {
+        public static int Sum(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public static int Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        public static int Multiplication(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+        public static int Division(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+    }
+}
---
+++ Task/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isTrue1 = false;
+            bool isTrue2 = false;
+            bool isTrue3 = false;
+            int num1 = 0;
+            int num2 = 0;
+            char op = 'a';
+
+            while (!isTrue1)
+            {
+                Console.Write("Enter The First Number: ");
+                if(int.TryParse(Console.ReadLine(), out num1))
+                {
+                    isTrue1 = true;
+                    while(!isTrue2)
+                    {
+                        Console.Write("Enter The Operator [+, -, *, /]: ");
+                        char[] ops = ['+', '-', '*', '/'];
+                        if (char.TryParse(Console.ReadLine(), out op) && ops.Contains(op))
+                        {
+                            isTrue2 = true;
+                            while (!isTrue3)
+                            {
+                                Console.Write("Enter The Second Number: ");
+                                if (int.TryParse(Console.ReadLine(), out num2))
+                                {
+                                    isTrue3 = true;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            MathDelegate mathDelegate = op switch
+            {
+                '+' => MathFunctions.Sum,
+                '-' => MathFunctions.Subtract,
+                '*' => MathFunctions.Multiplication,
+                '/' => MathFunctions.Sum
+            };
+
+
+            Console.WriteLine($"Result = {mathDelegate(num1, num2)}");
+
+        }
+    }
+}
